e6b67ff78afceac8382c324ca7f0a3ba31f9e77a
Rogerio-17 <rogeriojmf10@gmail.com>
2024-08-29 22:05:26 -0300

    create: testes unitarios

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ GymPass style app
 
 ## RFs
 
-- [ ] Deve ser possível se cadastrar;
+- [x] Deve ser possível se cadastrar;
 - [ ] Deve ser possível se autenticar;
 - [ ] Deve ser possível obter o perfil de um usuario logado;
 - [ ] Deve ser possível obter o número de check-ins realizados pelo usuário logado;
@@ -17,7 +17,7 @@ GymPass style app
 
 ## RNs
 
-- [ ] O usuario não pode se cadastrar com um email já cadastrado;
+- [x] O usuario não pode se cadastrar com um email já cadastrado;
 - [ ] O usuario não pode fazer 2 check-ins no mesmo dia;
 - [ ] O usuario não pode fazer check-in se não estiver perto (100m) da academia;
 - [ ] O check-in só pode ser válidado até 20 minutos após criado;
@@ -26,7 +26,7 @@ GymPass style app
 
 ## RNFs
 
-- [ ] A senha do usuario precisar estar criptografada;
-- [ ] Os dados da aplicação precisam estar persistidos em um banco PostgreSQL;
+- [x] A senha do usuario precisar estar criptografada;
+- [x] Os dados da aplicação precisam estar persistidos em um banco PostgreSQL;
 - [ ] Todas listas de dados precisam estar páginadas com 20 itens por página;
 - [ ] O usuario deve ser identificado por um JWT (JSON Web Token);
---
+++ src/use-cases/register.spec.ts
@@ -0,0 +1,61 @@
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
+import { expect, it, test } from "vitest";
+import { RegisterUseCase } from "./register";
+import { compare } from "bcryptjs";
+import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { describe } from "node:test";
+import { UserAlreadyExistsError } from "./errors/user-already-exists-error";
+
+describe("Register Use Case", () => {
+  it("should be able to register", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerUserCase = new RegisterUseCase(usersRepository);
+
+    const { user } = await registerUserCase.execute({
+      name: "John Doe",
+      email: "teste@teste.com",
+      password: "123456",
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+
+  it("should hash user password upon registration", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerUserCase = new RegisterUseCase(usersRepository);
+
+    const { user } = await registerUserCase.execute({
+      name: "John Doe",
+      email: "teste@teste.com",
+      password: "123456",
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      "123456",
+      user.password_hash
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
+
+  it("should not be able to register with sema email twice", async () => {
+    const usersRepository = new InMemoryUsersRepository();
+    const registerUserCase = new RegisterUseCase(usersRepository);
+
+    const email = "teste@teste.com";
+
+    await registerUserCase.execute({
+      name: "John Doe",
+      email,
+      password: "123456",
+    });
+
+    await expect(() =>
+      registerUserCase.execute({
+        name: "John Doe",
+        email,
+        password: "123456",
+      })
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError);
+  });
+});
