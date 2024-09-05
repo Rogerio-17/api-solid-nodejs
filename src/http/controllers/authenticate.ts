c2e13bcc5b15130438129903009b6f995615aa8d
Rogerio-17 <rogeriojmf10@gmail.com>
2024-09-05 17:02:49 -0300

    feat: adiciona criação de token jwt e a validção para rotas que necessitam do login do usuario

--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -2,6 +2,7 @@ import { FastifyRequest, FastifyReply } from "fastify";
 import { z } from "zod";
 import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-error";
 import { makeAuthenticateUseCase } from "@/use-cases/factories/make-authenticate-use-case";
+import { sign } from "crypto";
 
 export async function authenticate(
   request: FastifyRequest,
@@ -17,9 +18,22 @@ export async function authenticate(
   try {
     const authenticateUseCase = makeAuthenticateUseCase();
 
-    await authenticateUseCase.execute({
+    const { user } = await authenticateUseCase.execute({
       email,
       password,
+    });
+
+    const token = await reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      }
+    );
+
+    return reply.status(200).send({
+      token,
     });
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
@@ -28,6 +42,4 @@ export async function authenticate(
 
     throw err;
   }
-
-  return reply.status(200).send();
 }
